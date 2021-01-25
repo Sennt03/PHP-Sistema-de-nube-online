7f36c68fe3353547569ed852b97b485f9639734d
David <davidrsmk144@gmail.com>
2021-01-24 22:39:59 -0500

    Cambio de base_url

--- config/parametros.php
+++ config/parametros.php
@@ -1,5 +1,5 @@
 <?php 
-    define('base_url', 'http://localhost/senntcloud/');
+    define('base_url', 'https://senntcloud.herokuapp.com/');
     define('controller_default', 'userController');
     define('action_default', 'login');
 ?>
